f97f9e0347e6c4fccced2f3f32fd4ec859669f75
NeilTheSeal <neil.hendren@gmail.com>
2024-02-14 09:24:35 -0700

    Pass average grade spec

--- lib/student.rb
+++ lib/student.rb
@@ -15,4 +15,15 @@ class Student
   def log_score(score)
     @scores << score
   end
+
+  def grade
+    total_score = 0.0
+    average_score = 0.0
+    @scores.each_with_index do |score, number_of_tests|
+      score = Float(score)
+      total_score += score
+      average_score = total_score / (number_of_tests + 1)
+    end
+    average_score
+  end
 end

28dd39c99c7ee359d7def5ba5558a3261d4c3788
NeilTheSeal <neil.hendren@gmail.com>
2024-02-14 09:30:49 -0700

    Add initial specs for Course class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -1,5 +1,15 @@
-require 'rspec'
-require './lib/course'
-require './lib/student'
+require "rspec"
+require "./lib/course"
+require "./lib/student"
 
+RSpec.describe Course do
+  before(:each) do
+    @course = Course.new("Calculus", 2)
+    @student1 = Student.new({ name: "Morgan", age: 21 })
+    @student2 = Student.new({ name: "Jordan", age: 29 })
+  end
 
+  it "exists" do
+    expect(@course).to be_an_instance_of(Course)
+  end
+end

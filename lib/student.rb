87a8a6a87c6efc432a310376217891e9423cd8f6
NeilTheSeal <neil.hendren@gmail.com>
2024-02-14 09:17:58 -0700

    Pass all initialization specs

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
+# Student class
+#
+# Syntax:
+#
+# student = Student.new({ name: "Biff", age: 75 })
 class Student
+  attr_reader :name, :age, :scores
 
+  def initialize(info)
+    @name = info[:name]
+    @age = info[:age]
+    @scores = []
+  end
 end

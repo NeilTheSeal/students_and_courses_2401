e3a361e7d4caa06f965384ec899b1a4c3281e7dc
NeilTheSeal <neil.hendren@gmail.com>
2024-02-14 09:45:10 -0700

    Add initial spec for Gradebook class

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -1,5 +1,14 @@
-require 'rspec'
-require './lib/gradebook'
-require './lib/student'
-require './lib/course'
+require "rspec"
+require "./lib/gradebook"
+require "./lib/student"
+require "./lib/course"
 
+RSpec.describe "Gradebook" do
+  before(:each) do
+    @gradebook = Gradebook.new(instructor)
+  end
+
+  it "exists" do
+    expect(@gradebook).to be_an_instance_of(Gradebook)
+  end
+end

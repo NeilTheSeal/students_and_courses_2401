50b0e4f2d810d9bd452208cd45113aecd048f75f
NeilTheSeal <neil.hendren@gmail.com>
2024-02-14 09:49:51 -0700

    Add skeleton for Gradebook class

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,23 @@
+# Gradebook class - a container for Course and Student objects that tracks
+# student test scores across different courses. There are methods to list all
+# students for each class and provide a list of students below a certain
+# grade threshold
+#
+# Syntax:
+#
+# gradebook = Gradebook.new("Instructor name")
+#
+# Methods:
+# gradebook.instructor
+# gradebook.courses
+# gradebook.add_course(Course.new(...))
+# gradebook.list_all_students
+# gradebook.students_below(<int || float>threshold)
 class Gradebook
-  
+  attr_reader :instructor, :courses
+
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
 end

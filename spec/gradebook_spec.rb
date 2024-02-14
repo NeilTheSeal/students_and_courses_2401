4b437cf798a4999f481c30ffbacb13cbaaa39824
NeilTheSeal <neil.hendren@gmail.com>
2024-02-14 09:56:55 -0700

    Add specs for attributes at initialization

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe "Gradebook" do
   it "exists" do
     expect(@gradebook).to be_an_instance_of(Gradebook)
   end
+
+  describe "#initialization" do
+    it "has an instructor" do
+      expect(@gradebook.instructor).to eq("Stephen Hawking")
+    end
+    it "has a list of courses" do
+      expect(@gradebook.courses).to eq([])
+    end
+  end
 end

88bdb36ff32172ba17ad51e3efff7bc2912aae45
NeilTheSeal <neil.hendren@gmail.com>
2024-02-14 09:31:18 -0700

    Make skeleton for Course class

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,23 @@
+# Class Course - a course that takes on students until it has reached capacity.
+# When full, it will give course.full? = true and no longer take on students
+#
+# Sytax:
+#
+# course = Course.new("Course Name", <int>student_capacity)
+#
+# Methods:
+#
+# course.name
+# course.capacity
+# course.students
+# course.full?
+# course.enroll(Student.new({...}))
 class Course
- 
+  attr_reader :name, :capacity, :students
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
 end

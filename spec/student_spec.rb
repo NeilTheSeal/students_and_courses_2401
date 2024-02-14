4a42921dbff5af83ee12bd55fdd94ac4522fd394
NeilTheSeal <neil.hendren@gmail.com>
2024-02-14 09:14:41 -0700

    Add spec to check name at initialization

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,11 +1,19 @@
-require 'rspec'
-require './lib/student'
+require "rspec"
+require "./lib/student"
 
 RSpec.describe Student do
+  before(:each) do
+    @student = Student.new({ name: "Morgan", age: 21 })
+  end
+
   it "exists" do
-    morgan = Student.new({name: "Morgan", age: 21})
+    morgan = Student.new({ name: "Morgan", age: 21 })
     expect(morgan).to be_instance_of(Student)
   end
 
-  
+  describe "#check attributes at initialization" do
+    it "has a name" do
+      expect(@student.name).to eq("Morgan")
+    end
+  end
 end
